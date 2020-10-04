3bb3f62da9a925a8bc883de3b430ecb3757f2d84
Huy Tran <kingbazoka@gmail.com>
2020-10-04 02:02:41 -0700

    use PORT env

--- index.js
+++ index.js
@@ -106,6 +106,6 @@ io.on('connection', async (socket) => {
 
 app.use('/', express.static('./'));
 
-http.listen(3000, () => {
-  console.log('listening on *:3000');
+http.listen(process.env.PORT || 3000, () => {
+  console.log('listening...');
 });
